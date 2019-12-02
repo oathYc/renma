47e537e66b89d103c9e2e9118da09aee9e1e0ccb
oathYc <1316796206@qq.com>
2019-12-02 12:25:25 +0800

    api code

--- modules/content/models/Product.php
+++ modules/content/models/Product.php
@@ -16,13 +16,13 @@ class Product extends ActiveRecord
      * 获取商品的评价
      */
     public static function getComment($productId,$page=1,$pageSize=10){
-        if(!$page){$page=1;}
-        $limit = " limit ".($pageSize*($page-1)).",$pageSize";
+//        if(!$page){$page=1;}
+//        $limit = " limit ".($pageSize*($page-1)).",$pageSize";
         $sql = "select o.id as orderId,o.uid,m.nickname,m.avatar,o.productId,o.evaluate,FROM_UNIXTIME(o.evalTime, '%Y-%m-%d %H:%i:%s') as evalTime from {{%order}} o inner join {{%product}} p on p.id = o.productId left join {{%member}} m on m.id = o.uid where o.productId = $productId and evalTime > 0 order by evalTime desc ";
-        $total = \Yii::$app->db->createCommand($sql)->queryAll();
-        $total = count($total);
-        $sql .= $limit;
         $data = \Yii::$app->db->createCommand($sql)->queryAll();
+        $total = count($data);
+//        $sql .= $limit;
+//        $data = \Yii::$app->db->createCommand($sql)->queryAll();
         return ['total'=>$total,'comment'=>$data];
     }
 }
