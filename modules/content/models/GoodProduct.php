3e8a6f0b66ff9ba18f8d05b757b60a2a9072c027
oathYc <1316796206@qq.com>
2019-12-04 18:31:45 +0800

    ordernumber

--- modules/content/models/GoodProduct.php
+++ modules/content/models/GoodProduct.php
@@ -18,7 +18,14 @@ class GoodProduct extends ActiveRecord
     public static function getGoodProduct($page=1,$pageSize = 10){
         $total = GoodProduct::find()->count();
         $offset = ($page-1)*$pageSize;
-        $goodProduct = self::find()->orderBy('rank desc')->offset($offset)->limit($pageSize)->all();
+        $goodProduct = self::find()->orderBy('rank desc')->offset($offset)->limit($pageSize)->asArray()->all();
+        foreach($goodProduct as $k => $v){
+            $product = Product::find()->where("id = {$v['productId']}")->asArray()->one();
+            $goodProduct[$k]['title'] = $product['title'];
+            $goodProduct[$k]['image'] = $product['headMsg'];
+            $goodProduct[$k]['price'] = $product['price'];
+            $goodProduct[$k]['brand'] = $product['brand'];
+        }
         return ['goodTotal'=>$total,'goodProduct'=>$goodProduct];
     }
 }
