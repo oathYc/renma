5dcf6f0b69bc68b9e0c2bd3fb968bf465eb928a9
oathYc <1316796206@qq.com>
2019-12-06 09:53:16 +0800

    comment

--- modules/content/models/Product.php
+++ modules/content/models/Product.php
@@ -32,27 +32,45 @@ class Product extends ActiveRecord
     public static function getCommentByType($productId,$type=0,$page=1,$pageSize=10){
         if(!$page){$page=1;}
         $limit = " limit ".($pageSize*($page-1)).",$pageSize";
-        $where = " o.productId = $productId and o.evalTime > 0 ";
+        $where = " where o.productId = $productId and o.evalTime > 0 ";
         $order = "  order by o.evalTime desc ";
-        if($type ==0){
-            $today = strtotime(date('Y-m-d'));
-            $where .= " and o.evalTime >= $today";
+        $today = strtotime(date('Y-m-d'));
+        $where1 = "$where and o.evalTime >= $today";//最新
+        $where2 = "$where and !isnull(o.evalImage)";//有图
+        $where3 = "$where and !isnull(o.evalVideo)";//视频
+        if($type ==1){
+            $where = $where1;
         }
         if($type ==2){
-            $where .= " and !isnull(o.evalImage)";
+            $where = $where2;
         }
         if($type == 3){
-            $where .= " and !isnull(o.evalVideo)";
+            $where = $where3;
         }
-        $sql = "select o.id as orderId,o.uid,m.nickname,m.avatar,o.productId,o.evaluate,FROM_UNIXTIME(o.evalTime, '%Y-%m-%d %H:%i:%s') as evalTime,o.evalImage,o.evalVideo from {{%order}} o inner join {{%product}} p on p.id = o.productId left join {{%member}} m on m.id = o.uid where $where $order ";
-        $data = \Yii::$app->db->createCommand($sql)->queryAll();
-        $total = count($data);
-        $sql .= $limit;
-        $data = \Yii::$app->db->createCommand($sql)->queryAll();
+        $sql = "select o.id as orderId,o.uid,m.nickname,m.avatar,o.productId,o.evaluate,FROM_UNIXTIME(o.evalTime, '%Y-%m-%d %H:%i:%s') as evalTime,o.evalImage,o.evalVideo from {{%order}} o inner join {{%product}} p on p.id = o.productId left join {{%member}} m on m.id = o.uid  ";
+        $sqlData = $sql.$where.$order.$limit;
+        $data = \Yii::$app->db->createCommand($sqlData)->queryAll();
         foreach($data as $k => $v){
             $data[$k]['evalImage'] = $v['evalImage']?unserialize($v['evalImage']):'';
             $data[$k]['evalVideo'] = $v['evalVideo']?unserialize($v['evalVideo']):'';
         }
-        return ['total'=>$total,'comment'=>$data];
+        //获取类型数量
+        $sql1 = $sql.$where1;
+        $content1 = \Yii::$app->db->createCommand($sql1)->queryAll();
+        $count1 = count($content1);
+        $sql2 = $sql.$where2;
+        $content2 = \Yii::$app->db->createCommand($sql2)->queryAll();
+        $count2 = count($content2);
+        $sql3 = $sql.$where3;
+        $content3 = \Yii::$app->db->createCommand($sql3)->queryAll();
+        $count3 = count($content3);
+        $count = $count1+$count2+$count3;
+        $counts= [
+            ['type'=>0,'number'=>$count],
+            ['type'=>1,'number'=>$count1],
+            ['type'=>2,'number'=>$count2],
+            ['type'=>3,'number'=>$count3],
+        ];
+        return ['count'=>$counts,'comment'=>$data];
     }
 }
