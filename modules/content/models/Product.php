b79f8cc50031fbb0eeacc12c5223dd48c5024a03
oathYc <1316796206@qq.com>
2019-12-06 09:41:15 +0800

    product comment

--- modules/content/models/Product.php
+++ modules/content/models/Product.php
@@ -32,15 +32,19 @@ class Product extends ActiveRecord
     public static function getCommentByType($productId,$type=0,$page=1,$pageSize=10){
         if(!$page){$page=1;}
         $limit = " limit ".($pageSize*($page-1)).",$pageSize";
-        $where = " o.productId = $productId and evalTime > 0 ";
-        $order = "  order by evalTime desc ";
+        $where = " o.productId = $productId and o.evalTime > 0 ";
+        $order = "  order by o.evalTime desc ";
+        if($type ==0){
+            $today = strtotime(date('Y-m-d'));
+            $where .= " and o.evalTime >= $today";
+        }
         if($type ==2){
             $where .= " and !isnull(o.evalImage)";
         }
         if($type == 3){
             $where .= " and !isnull(o.evalVideo)";
         }
-        $sql = "select o.id as orderId,o.uid,m.nickname,m.avatar,o.productId,o.evaluate,FROM_UNIXTIME(o.evalTime, '%Y-%m-%d %H:%i:%s') as evalTime from {{%order}} o inner join {{%product}} p on p.id = o.productId left join {{%member}} m on m.id = o.uid where $where $order ";
+        $sql = "select o.id as orderId,o.uid,m.nickname,m.avatar,o.productId,o.evaluate,FROM_UNIXTIME(o.evalTime, '%Y-%m-%d %H:%i:%s') as evalTime,o.evalImage,o.evalVideo from {{%order}} o inner join {{%product}} p on p.id = o.productId left join {{%member}} m on m.id = o.uid where $where $order ";
         $data = \Yii::$app->db->createCommand($sql)->queryAll();
         $total = count($data);
         $sql .= $limit;
