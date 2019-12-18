7ce782266c4ffe18bde062aea031d6b0c383fc32
oathYc <1316796206@qq.com>
2019-12-18 22:57:15 +0800

    integral

--- modules/content/controllers/ApiController.php
+++ modules/content/controllers/ApiController.php
@@ -1511,7 +1511,9 @@ class ApiController extends  Controller
         $userCou = UserCoupon::find()->where("uid = $uid and status = 0")->count();
         //免单数量
         $feeCount = Order::find()->where("uid = $uid and status = 1 and serverFee = 0")->count();
-        $data = ['id'=>$uid,'member'=>$member,'endTime'=>$endTime,'money'=>100,'reduceMoney'=>$reduceMoney?$reduceMoney:0,'userCoupon'=>$userCou,'feeCount'=>$feeCount];
+        //优惠卷兑换
+        $coupons = Coupon::find()->asArray()->all();
+        $data = ['id'=>$uid,'member'=>$member,'endTime'=>$endTime,'money'=>100,'reduceMoney'=>$reduceMoney?$reduceMoney:0,'userCoupon'=>$userCou,'feeCount'=>$feeCount,'coupons'=>$coupons];
         Methods::jsonData(1,'success',$data);
     }
     /**
