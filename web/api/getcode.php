4f421f84dd0b23ed818632e312b52aa621fe4532
oathYc <1316796206@qq.com>
2020-02-27 17:50:55 +0800

    getcode

--- web/api/getcode.php
+++ web/api/getcode.php
@@ -1,14 +1,7 @@
 <?php
 include('./phpqrcode.php');
-//获取系统设置的图片
-$domain = 'https://lck.hzlyzhenzhi.com';
-$url = "$domain/content/api/get-qrcode-img";
-$img = file_get_contents($url);
-if(!$img){//不存在获取默认图片
-    $imgpath="./backgroud.jpg"; //背景图片
-}else{
-    $imgpath = str_replace("$domain",'./..',$img);
-}
+
+$imgpath="./backgroud.jpg"; //背景图片
 //$imgpath="./bg.jpg"; //背景图片
 $im=imagecreatefromjpeg($imgpath);
 //背景中二维码的位置和尺寸信息
@@ -19,12 +12,12 @@ $height=180;
 
 //生成我们需要的二维码图片
 $url="pages/index/index?code=".$_GET["code"];
-$url="$domain/content/login/share?uid=".$_GET["uid"];
+$url="https://lck.hzlyzhenzhi.com/content/login/share?uid=".$_GET["uid"];
 
 QRcode::png($url,false, 'L', 6, 1);
 
 $ob_contents = ob_get_contents();
-ob_end_clean(); 
+ob_end_clean();
 
 $qrim = imagecreatefromstring($ob_contents);
 
